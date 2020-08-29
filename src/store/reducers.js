e7eb07d749f5dde873a80e0e0492b72cae48aa7c
Prateek Diwedi <diwedi1112@gmail.com>
2020-08-29 15:06:34 -0700

    created reducers and passed tests

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,28 +1,47 @@
-import C from '../constants';
+import C from '../constants'
 
-export const goal = (state = 10, action) =>
-  (action.type === C.SET_GOAL) ?
-    parseInt(action.payload) :
-    state
+export const goal = (state=10, action) => 
+	(action.type === C.SET_GOAL) ? 
+		 parseInt(action.payload) :
+		 state
 
-export const skiDay = (state = null, action) =>
+export const skiDay = (state=null, action) => 
   (action.type === C.ADD_DAY) ?
-    action.payload :
-    state
+  	action.payload :
+  	state
 
-export const errors = (state = [], action) => {
-
-  switch (action.type) {
-    case C.ADD_ERROR:
-
-      return [
-        ...state,
-        action.payload
-      ]
-
-    case C.CLEAR_ERROR:
-
+export const errors = (state=[], action) => {
+  switch(action.type) {
+    case C.ADD_ERROR :
+    	return [
+         ...state,
+         action.payload
+    	]
+    case C.CLEAR_ERROR : 
       return state.filter((message, i) => i !== action.payload)
+  	default: 
+  		return state
+  }
+}
+
+export const allSkiDays = (state=[], action) => {
+
+  switch(action.type) {
+
+    case C.ADD_DAY : 
+
+      const hasDay = state.some(skiDay => skiDay.date === action.payload.date)
+
+      return (hasDay) ?
+         state :
+         [
+           ...state,
+           skiDay(null, action)
+         ].sort((a, b) => new Date(b.date) - new Date(a.date))
+
+    case C.REMOVE_DAY :
+
+      return state.filter(skiDay => skiDay.date !== action.payload)     
 
     default:
       return state
@@ -30,24 +49,33 @@ export const errors = (state = [], action) => {
 
 }
 
-export const allSkiDays = (state, action) => {
-  switch (action.type) {
 
-    case C.ADD_DAY:
+export const fetching = (state = false, action) => {
+  switch(action.type) {
+    case C.FETCH_RESORT_NAMES :
+    return true
 
-    const hasAlready = state.some( skiDay => skiDay.date === action. payload.date)
-      return (hasAlready) ?
-        state :
-        [
-          ...state,
-          skiDay(null, action)
-        ]
+    case C.CANCEL_FETCHING :
+      return false
 
-        case C.REMOVE_DAY:
+    case C.CHANGE_SUGGESTIONS :
+      return false
 
-        return state.filter(skiDay => skiDay.date !== action.payload)
+    default: 
+    return state
+  }
+}
+
+export const suggestions = (state = [], action) => {
+  switch(action.type) {
+
+    case C.CLEAR_SUGGESTIONS :
+      return []
+
+      case C.CHANGE_SUGGESTIONS :
+        return action.payload
 
     default:
-      return state
+      return state;
   }
 }
